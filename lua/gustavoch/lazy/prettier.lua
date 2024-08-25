94a7f547185b01314af6b69f4171f3358f72fdc3
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-08-25 13:44:50 +0100

    fix prettier keymap

--- lua/gustavoch/lazy/prettier.lua
+++ lua/gustavoch/lazy/prettier.lua
@@ -1,3 +1,9 @@
 return {
-    'prettier/vim-prettier'
+    'prettier/vim-prettier',
+    run = 'yarn install',
+    config = function()
+        vim.api.nvim_del_keymap('n', '<leader>p')
+        vim.api.nvim_set_keymap('n', '<leader>pf', ':Prettier<CR>',
+            { noremap = true, silent = true, desc = "Format using Prettier" })
+    end
 }
