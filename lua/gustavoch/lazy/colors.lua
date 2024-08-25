5a81a06069c09d276964764614a80128c285f749
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-08-25 13:41:46 +0100

    use tokyonight colors

--- lua/gustavoch/lazy/colors.lua
+++ lua/gustavoch/lazy/colors.lua
@@ -1,26 +1,25 @@
 function ColorMyPencils(color)
+    color = color or "tokyonight"
+    vim.cmd.colorscheme(color)
 
-	color = color or "dracula"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
     {
-    "Mofiqul/dracula.nvim",
-    name = "dracula",
-    lazy = false,
-    opts = {},
-    config = function()
-        ColorMyPencils()
-    end
+        "Mofiqul/dracula.nvim",
+        name = "dracula",
+        lazy = false,
+        opts = {},
     },
     {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    lazy = false,
-    opts = {},
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
+        lazy = false,
+        opts = {},
+        config = function()
+            ColorMyPencils()
+        end
     }
 }
