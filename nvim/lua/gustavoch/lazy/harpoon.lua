e1a066f674d300e7596fd3b991e934a3dcb155b4
Gustavo Francelino <gustavo.francelino@eliostalent.com>
2025-09-27 10:23:59 -0300

    feat: upgrade to Harpoon v2
    - Switch from harpoon v1 to v2 with proper branch specification
    - Update API calls to use new harpoon:list() methods
    - Add dependency on plenary.nvim
    - Include additional navigation keybindings (Ctrl+Shift+P/N)
    - Add descriptive labels for better which-key integration
    - Add success notification for debugging

--- nvim/lua/gustavoch/lazy/harpoon.lua
+++ nvim/lua/gustavoch/lazy/harpoon.lua
@@ -1,16 +1,28 @@
 return {
-    'ThePrimeagen/harpoon',
-
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+        local harpoon = require("harpoon")
+        
+        -- Setup harpoon with default settings
+        harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+        -- Key mappings for Harpoon v2
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle menu" })
 
-        vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+        -- Navigate to specific files
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon: Go to file 1" })
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon: Go to file 2" })
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon: Go to file 3" })
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon: Go to file 4" })
+        
+        -- Toggle previous & next buffers stored within Harpoon list
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon: Previous file" })
+        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Harpoon: Next file" })
+        
+        vim.notify("Harpoon v2 loaded successfully", vim.log.levels.INFO)
     end
 }
